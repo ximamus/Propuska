8c4cdea3989e8ffb33cea50c35cc1a68cdfc5cb9
ximamus <ximamus@gmail.com>
2021-10-29 20:24:24 +0500

    Добавьте файлы проекта.

---
+++ Lib/Prop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class Prop
+    {
+        private int number;
+        private string fio;
+        private int type;
+        private DateTime data;
+
+
+        public Prop(int number, string fio, int type, DateTime data) {
+            this.number = number;
+            this.fio = fio;
+            this.type = type;
+            this.data = data;
+        }
+
+        
+    }
+}
---
+++ Propusk/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib;
+
+namespace Propusk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Prop> NewList = new List<Prop>();
+            string menu = "Программа работы с пропусками\n" +
+            "=============================\n" +
+            "Выберите пункт меню:\n" +
+            "1. Добавить пропуск\n" +
+            "2. Удалить пропуск\n" +
+            "3. Дата последнего выданного пропуска\n" +
+            "4. Пропуска за промежуток";
+
+            Console.WriteLine(menu);
+            while (true) {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+
+                        Console.WriteLine("");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine(menu);
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
+
+
+        }
+    }
+}
