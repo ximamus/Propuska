849bb3d34fa8ba9c31983948e1300eb66145a450
ximamus <ximamus@gmail.com>
2021-10-30 13:04:40 +0500

    end

--- Lib/Prop.cs
+++ Lib/Prop.cs
@@ -9,18 +9,57 @@ namespace Lib
     public class Prop
     {
         private int number;
-        private string fio;
+        public string fio;
         private int type;
-        private DateTime data;
+        public DateTime data;
 
-
-        public Prop(int number, string fio, int type, DateTime data) {
+        public Prop(int number, string fio, int type, DateTime data)
+        {
             this.number = number;
             this.fio = fio;
             this.type = type;
             this.data = data;
         }
+    }
 
-        
+    public class Props
+    {
+        public List<Prop> Propuska = new List<Prop>();
+
+        public void Dobav(string name, int type) {
+            Propuska.Add(new Prop(Propuska.Count + 1, name, type, DateTime.Now));
+            Console.WriteLine("Пропуск введен\n" +
+                "Нажмите любую клавишу для продолжения");
+            Console.ReadKey();
+        }
+
+        public void DelName(string nameDel) {
+            foreach (Prop prop in Propuska) {
+                if (prop.fio.Contains(nameDel)) {
+                    Console.WriteLine($"Пропуск для {prop.fio} удален");
+                    Propuska.Remove(prop);
+                    Console.WriteLine("Нажмите любую клавишу для продолжения");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+
+        public DateTime LastDate {
+            get {
+                Prop lastData = Propuska.Last();
+                return lastData.data; 
+            }
+        }
+
+        public int Amount(DateTime date1, DateTime date2) {
+            int amount = 0;
+            foreach (Prop prop in Propuska) {
+                if (DateTime.Compare(prop.data, date1) > 0 && DateTime.Compare(prop.data, date2) < 0) {
+                    amount++;
+                }
+            }            
+            return amount;
+        }
     }
 }
--- Propusk/Program.cs
+++ Propusk/Program.cs
@@ -10,35 +10,68 @@ namespace Propusk
     class Program
     {
         static void Main(string[] args)
-        {
-            List<Prop> NewList = new List<Prop>();
+        {             
+            Props props = new Props();
             string menu = "Программа работы с пропусками\n" +
             "=============================\n" +
             "Выберите пункт меню:\n" +
             "1. Добавить пропуск\n" +
             "2. Удалить пропуск\n" +
             "3. Дата последнего выданного пропуска\n" +
-            "4. Пропуска за промежуток";
+            "4. Пропуска за промежуток\n" +
+            "5. Выход из программы";
 
-            Console.WriteLine(menu);
             while (true) {
+                Console.Clear();
+                Console.WriteLine(menu);
                 switch (Console.ReadLine())
                 {
                     case "1":
-
-                        Console.WriteLine("");
+                        bool dataSet = false;
+                        while (!dataSet)
+                        {   
+                            
+                            Console.WriteLine("Введите ФИО");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Введите тип пропуска: 1 - обычный, 2 - срочный, 3 - транзит");
+                            int type = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Данные верны?\n" +
+                                $"Номер пропуска: {props.Propuska.Count + 1}\n" +
+                                $"ФИО: {name}\n" +
+                                $"Тип пропуска: {type}\n" +
+                                $"Дата пропуска: {DateTime.Now}\n" +
+                                $"Введите y - да, n - нет");
+                            if (Console.ReadLine().ToLower() == "y") {
+                                props.Dobav(name, type);                                
+                                dataSet = true;                                
+                            }                           
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Введите фамилию человека, чей пропуск нужно удалить");
+                        string nameDel = Console.ReadLine();
+                        props.DelName(nameDel);
+                        break;
+                    case "3":
+                        Console.WriteLine($"Дата последнего выданного пропуска: {props.LastDate.ToString("d")}\n" +
+                            "Нажмите любую клавишу для продолжения");
                         Console.ReadKey();
                         break;
-                    default:
-                        Console.Clear();
-                        Console.WriteLine(menu);
+                    case "4":
+                        Console.WriteLine("Введите первую дату в формате дд/мм/гггг");
+                        DateTime date1 = DateTime.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите вторую дату в формате дд/мм/гггг");
+                        DateTime date2 = DateTime.Parse(Console.ReadLine());                        
+                        Console.WriteLine($"Количество пропусков, выданных между {date1.ToString("d")} и {date2.ToString("d")}: {props.Amount(date1, date2)}\n" +
+                            "Нажмите любую клавишу для продолжения");
                         Console.ReadKey();
+                        break;
+                    case "5":
+                        return;
+                    default:                        
                         break;
                 }
-
             }
-
-
         }
     }
 }
